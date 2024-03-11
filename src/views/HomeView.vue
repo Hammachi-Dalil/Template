e256a95701ba8a6d222928bc898571a1e178967f
Hammachi-Dalil <116789242+Hammachi-Dalil@users.noreply.github.com>
2024-03-11 12:15:24 +0100

    tailwind fonctionne

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-center text-red-800">Home</h1>
   </main>
 </template>
